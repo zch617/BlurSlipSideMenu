24f0fd5804ed1dee18c05db62d69e9651f456153
zhangcihang <zhangcihang@mofang.com>
2015-12-01 18:27:18 +0800

    Initial Commit

---
+++ BlurSlipSideMenuDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlurSlipSideMenuDemo
+//
+//  Created by 张慈航 on 15/12/1.
+//  Copyright (c) 2015年 com.zhangcihang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
